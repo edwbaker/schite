01e542d7197ef28b5eeb1bd4546d34fbe5ed8755
Ed Baker <edwbaker@gmail.com>
2022-07-31 22:33:21 +0100

    Documentation

--- R/cite_bibentry.R
+++ R/cite_bibentry.R
@@ -1,3 +1,8 @@
+#' Generate a citation from a bibentry
+#'
+#' Generate a citation for use with schite using a bibentry object.
+#' @param bibentry A bibentry object
+#' @param use Optional usage to add to the citation (e.g. 'Visualisation', 'Data Processing')
 #' @export
 cite_bibentry <- function(bibentry, use=NULL) {
   attr(bibentry, "use") <- use
--- R/cite_r_package.R
+++ R/cite_r_package.R
@@ -1,3 +1,9 @@
+#' Cite an R package
+#'
+#' Generate a citation for use with schite using the CITATION file
+#' within an R package.
+#' @param pkg The name of the package
+#' @param use Optional usage to add to the citation (e.g. 'Visualisation', 'Data Processing')
 #' @export
 #' @importFrom utils citation
 cite_r_package <- function(pkg, use=NULL) {
